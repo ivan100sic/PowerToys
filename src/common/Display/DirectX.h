88ca883e749155ecf60b57ae1433b5430a9967b8
Ivan Stošić <ivan100sic@gmail.com>
2021-01-25 15:39:12 +0100

    Make DeviceResources compile

--- src/common/Display/DirectX.h
+++ src/common/Display/DirectX.h
@@ -11,35 +11,11 @@
 
 namespace DX
 {
-    void ThrowIfFailed(HRESULT hr)
-    {
-        if (FAILED(hr))
-        {
-            throw hr;
-        }
-    }
+    void ThrowIfFailed(HRESULT hr);
 
-#if defined(_DEBUG)
     // Check for SDK Layer support.
-    inline bool SdkLayersAvailable()
-    {
-        HRESULT hr = D3D11CreateDevice(
-            nullptr,
-            D3D_DRIVER_TYPE_NULL, // There is no need to create a real hardware device.
-            0,
-            D3D11_CREATE_DEVICE_DEBUG, // Check for the SDK layers.
-            nullptr, // Any feature level will do.
-            0,
-            D3D11_SDK_VERSION, // Always set this to D3D11_SDK_VERSION for Windows Runtime apps.
-            nullptr, // No need to keep the D3D device reference.
-            nullptr, // No need to know the feature level.
-            nullptr // No need to keep the D3D device context reference.
-        );
-
-        return SUCCEEDED(hr);
-    }
-#endif
-
+    inline bool SdkLayersAvailable();
+    
     struct IDeviceNotify
     {
         virtual void OnDeviceLost() = 0;
@@ -48,6 +24,7 @@ namespace DX
 
     class DeviceResources
     {
+    public:
         DeviceResources();
 
         // Set output window target.
